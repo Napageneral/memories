cbad1badfc051c7b5261cb42c0764300f00c7759
Tyler Brandt <tnapathy@gmail.com>
2021-12-24 09:12:43 -0800

    remove keys

--- PythonServer/models/loader.py
+++ PythonServer/models/loader.py
@@ -5,8 +5,8 @@ import boto3
 def download_models():
     modelsPath = os.getcwd() + '/models/'
     s3 = boto3.client('s3',
-         aws_access_key_id="AKIAVCLQCUGXEXH7MNDD",
-         aws_secret_access_key= "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")
+         aws_access_key_id="KEY_ID",
+         aws_secret_access_key= "ACCESS_KEY")
 
     if not os.path.isfile(modelsPath + "1epoch.pth"):
         s3.download_file('ulmfitbucket', '1epoch.pth', modelsPath+'1epoch.pth')
